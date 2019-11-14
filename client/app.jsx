48312982309f78490e4e63e3c22a2dd721b4201f
Jason Chen <jasonchen@Jasons-MacBook-Pro.local>
2019-11-14 14:25:28 -0800

    webpack workshop

---
+++ client/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = (props) => {
+  return (
+    <div>
+      <div>Hello 124's</div>
+      <button>click me</button>
+
+    </div>
+  )
+}
+
+export default App;
